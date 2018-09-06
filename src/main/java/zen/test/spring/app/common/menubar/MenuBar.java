6f3e0d7daa440d2ba4861c5d20082ceec7c40310
Luca <l.zenobi@t4project.com>
2018-09-06 17:51:46 +0200

    Minor graphic changes

--- src/main/java/zen/test/spring/app/common/menubar/MenuBar.java
+++ src/main/java/zen/test/spring/app/common/menubar/MenuBar.java
@@ -1,6 +1,8 @@
 package zen.test.spring.app.common.menubar;
 
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.ParentLayout;
 import com.vaadin.flow.router.RouterLayout;
 import com.vaadin.flow.router.RouterLink;
@@ -10,11 +12,19 @@ import zen.test.spring.app.facilities.Facilities;
 @ParentLayout(MainLayout.class)
 public class MenuBar extends Div implements RouterLayout {
 
+  private HorizontalLayout menuElementsContainer = new HorizontalLayout();
+
   public MenuBar() {
-    add(
-        new RouterLink("Home", Facilities.class),
-        new RouterLink("Facilities", Facilities.class)
-    );
+    menuElementsContainer.setId("menuElementsContainer");
+    add(menuElementsContainer);
+
+    addMenuElement(Facilities.class, "Home");
+    addMenuElement(Facilities.class, "Facilities");
+  }
+
+  private void addMenuElement(Class<? extends Component> navigationTarget, String name) {
+    RouterLink link = new RouterLink(name, navigationTarget);
+    menuElementsContainer.add(new RouterLink(name, navigationTarget));
   }
 
 }
